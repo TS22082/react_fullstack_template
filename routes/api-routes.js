565c9bbe5746e227bce414e7a1d757615b8b2853
TS22082 <ts22082@gmail.com>
2020-04-11 16:44:31 -0700

    make route end point message and change name

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/example", (req, res) => {
+  console.log("The end-point was hit");
   res.send({ message: "Success" });
 });
 

e858012c94bf472ce29058ca728f6bef3b7a0965
TS22082 <ts22082@gmail.com>
2020-04-11 17:44:28 -0700

    complete request to backend and display on client

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React, { useEffect } from "react";
-import logo from "./logo.svg";
+import axios from "axios";
 import "./App.css";
 
 function App() {
   useEffect(() => {
-    console.log("Hello");
+    axios.get("/example").then((result) => {
+      console.log(result.data.message);
+    });
   }, []);
+
   return (
     <div className="App">
       <h1>A node express react starter template</h1>
